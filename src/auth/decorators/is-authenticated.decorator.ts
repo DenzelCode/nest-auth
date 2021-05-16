8f9731497ce3131deea67f0814c75e616e97d0cb
Denzel Code <denzelcodedev@gmail.com>
2021-05-16 17:51:34 -0400

    fix: unnecesary file removed

--- src/auth/decorators/is-authenticated.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const IsAuthenticated = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-
-    return !!request.user;
-  },
-);
