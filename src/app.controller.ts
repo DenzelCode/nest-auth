c02b9a7c51fce54013ad79009c184c6836404b90
Denzel Code <denzelcodedev@gmail.com>
2021-08-07 05:39:23 -0400

    test: testing location added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,16 +5,21 @@ import { hostname } from 'os';
 @Controller()
 export class AppController {
   @Get()
-  main(@Req() req: Request): string {
+  main(@Req() req: Request) {
     return this.getContainer(req);
   }
 
   @Get('test')
-  test(@Req() req: Request): string {
+  test(@Req() req: Request) {
     return this.getContainer(req);
   }
 
   private getContainer(req: Request) {
-    return `${req.ip} - ${hostname()}`;
+    return {
+      ip: req.ip,
+      forwardedFor: req.headers['x-forwarded-for'],
+      realIp: req.headers['x-real-ip'],
+      container: hostname(),
+    };
   }
 }
