11e35d9a673b489e18dfe9afaa17293f9acb07c9
Denzel Code <denzelcodedev@gmail.com>
2021-08-07 21:12:26 -0400

    chore: nginx updated

--- src/core/adapter/custom-socket-io.adapter.ts
+++ src/core/adapter/custom-socket-io.adapter.ts
@@ -1,15 +1,9 @@
 import { Server, ServerOptions } from 'socket.io';
-import { getAddress } from '../../shared/utils/get-address';
 import { SocketIoAdapter } from './socket-io-adapter';
 
 export class CustomSocketIoAdapter extends SocketIoAdapter {
   createIOServer(port: number, options?: ServerOptions) {
     const server = super.createIOServer(port, options) as Server;
-
-    server.on(
-      'connection',
-      client => (client.handshake.address = getAddress(client)),
-    );
 
     return server;
   }
