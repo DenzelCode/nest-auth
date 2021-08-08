50bad20c5369875ac8a405b199b02ef0eb67a790
Denzel Code <denzelcodedev@gmail.com>
2021-08-07 21:27:57 -0400

    chore: nginx updated

--- src/core/adapter/custom-socket-io.adapter.ts
+++ src/core/adapter/custom-socket-io.adapter.ts
@@ -1,10 +1,3 @@
-import { Server, ServerOptions } from 'socket.io';
 import { ExternalSocketIoAdapter } from './external-socket-io-adapter';
 
-export class CustomSocketIoAdapter extends ExternalSocketIoAdapter {
-  createIOServer(port: number, options?: ServerOptions) {
-    const server = super.createIOServer(port, options) as Server;
-
-    return server;
-  }
-}
+export class CustomSocketIoAdapter extends ExternalSocketIoAdapter {}
