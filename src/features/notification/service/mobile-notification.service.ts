4152c04e28a68ea3f7ef556d63630e3dc334d4fc
Denzel Code <denzelcodedev@gmail.com>
2021-10-18 05:54:45 -0400

    test: testing socketio version

--- src/features/notification/service/mobile-notification.service.ts
+++ src/features/notification/service/mobile-notification.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { messaging } from 'firebase-admin';
 import { fcm } from '../api/firebase';
 
@@ -8,13 +8,7 @@ export class MobileNotificationService {
     token: string | string[],
     payload: messaging.MessagingPayload,
   ) {
-    for (const key of Object.keys(payload.data || {})) {
-      if (typeof payload.data[key] === 'object') {
-        delete payload.data[key];
-      }
-    }
-
-    new Logger().debug(`this is the payload: ${JSON.stringify(payload)}`);
+    delete payload.notification.data;
 
     return fcm.sendToDevice(token, payload);
   }
